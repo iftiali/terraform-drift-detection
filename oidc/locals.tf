f74d38a082d6557da5e61e2f76cce25b9541d72d
seunayolu <seunayolu@gmail.com>
2025-10-31 19:45:45 +0100

    oidc repo update

--- oidc/locals.tf
+++ oidc/locals.tf
@@ -1,7 +1,7 @@
 locals {
   github_repositories = [
     "seunayolu/terraform-github-oidc-drift-demo",
-    "seunayolu/terraform-files"
+    "seunayolu/terraform-drift-detection"
   ]
   github_repo_conditions = [
     for repo in local.github_repositories : "repo:${repo}:*"
